4abe75f2f9307f9afb296f338a03756962a5c3f5
benaitcheson <benaitcheson@hotmail.com>
2021-09-20 13:57:27 +1000

    kahoot music added

--- main.js
+++ main.js
@@ -12,4 +12,8 @@ questions.addEventListener('click', () => {
 })
 
 
-
+window.addEventListener("DOMContentLoaded", event => {
+    const audio = document.querySelector("audio");
+    audio.volume = 0.2;
+    audio.play();
+  });
