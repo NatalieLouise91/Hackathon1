f57fd94968dac598a1608cd1f31e0491d2e19db5
jkirky82 <jkirky82@gmail.com>
2021-09-20 14:08:54 +1000

    added JDOM for user options

--- main.js
+++ main.js
@@ -2,8 +2,16 @@
 let questions = document.getElementById('get-question')
 
 // Adding in user options here
+//number 
+// category 
+// Difficulty
 
 questions.addEventListener('click', () => {
+    //number = document.getElementById("number")
+    //category = document.getElementById("category")
+    //difficulty = document.getElementById("difficulty")
+    category_list = {"General Knowledge": 9,"Entertainment: Film": 11, "Entertainment: Music": 12,"Entertainment: Television": 14, "Entertainment: Video Games": 15 }
+    if (category == "")
     fetch('https://opentdb.com/api.php?amount=10')
     .then(res => res.json())
     .then(data => {
