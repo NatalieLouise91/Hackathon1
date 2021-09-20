f85f1c51276ed4df909f77c21e8f9e1e55a858f8
NatalieLouise91 <natalie936@hotmail.com>
2021-09-20 13:55:59 +1000

    added divs into index html file

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 // new js file
 let questions = document.getElementById('get-question')
+let questionsDiv = document.getElementById('questions')
 
 questions.addEventListener('click', () => {
     fetch('https://opentdb.com/api.php?amount=10')
     .then(res => res.json())
     .then(data => {
+        questionsDiv.innerHTML += `<p> Question: ${data.results[0].question} </p>`
+        questionsDiv.innerHTML += `<p> Correct Answer: ${data.results[0].correct_answer} </p>`
+        questionsDiv.innerHTML += `<p> Incorrect Answers: ${data.results[0].incorrect_answers} </p>`
         console.log(data)
     })
 })
-
-
-
